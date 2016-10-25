b5b737385e018db5cb9ecacbd9071c02dce519a6
Austin Macdonald <asmacdo@gmail.com>
2016-10-25 00:32:17 -0400

    Add a test app with nested routing
    Real plugins will be required to create Importers and Publishers that
    use nested routing, so should the test app. Also add a lot of comments
    to make it clear how to use this test app as a template for a real
    plugin.
    
    re #2369

--- app/pulp/app/tests/testapp/viewsets.py
+++ app/pulp/app/tests/testapp/viewsets.py
@@ -1,10 +1,27 @@
+"""
+Each model also requires a ViewSet so that they may be handled by the API. Again, inherit the
+matching base class from `pulp.app.viewsets`. `endpoint_name` should by convention match the type
+of the content returned by the queryset.
+"""
 from pulp.app import viewsets
 
-from pulp.app.tests.testapp.models import TestContent
-from pulp.app.tests.testapp.serializers import TestContentSerializer
+from pulp.app.tests.testapp.models import TestContent, TestImporter
+from pulp.app.tests.testapp.serializers import TestContentSerializer, TestImporterSerializer
 
 
 class TestContentViewSet(viewsets.ContentViewSet):
     endpoint_name = 'test'
     queryset = TestContent.objects.all()
     serializer_class = TestContentSerializer
+
+
+class TestImporterViewSet(viewsets.ImporterViewSet):
+    # The relative url is built using the endpoint name and the parent class's endpoint name, in
+    # this case importers/test/.
+    endpoint_name = 'test'
+    # If the urls are built using a field other than the pk, specify the field with `lookup_field`.
+    lookup_field = 'name'
+
+    queryset = TestImporter.objects.all()
+    serializer_class = TestImporterSerializer
+
