2b44220f9563b935d4b55f815e5409c269e3ebd1
Austin Macdonald <asmacdo@gmail.com>
2016-11-17 18:25:43 -0500

    fix by disabling drf optimization

--- app/pulp/app/serializers/fields.py
+++ app/pulp/app/serializers/fields.py
@@ -11,6 +11,13 @@ class ContentRelatedField(DetailRelatedField):
     lookup_field = 'pk'
     queryset = models.Content.objects.all()
 
+    def use_pk_only_optimization(self):
+        """
+        If the lookup field is `pk`, DRF substitutes a PKOnlyObject as an optimization. This
+        optimization breaks with Detail fields like this one which need access to their Meta
+        class to get the relevant `view_name`.
+        """
+        return False
 
 
 class RepositoryRelatedField(serializers.HyperlinkedRelatedField):
