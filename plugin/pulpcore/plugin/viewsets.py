8133b139672ea2978b9d3b59190335bd3be4f0f0
Austin Macdonald <austin@redhat.com>
2018-01-10 14:53:53 -0500

    Move sync dispatch to plugin API
    Modifies the plugin API, moving respoonsibility for sync from
    parent classes to the plugins.
    
    closes #3074
    https://pulp.plan.io/issues/3074

--- plugin/pulpcore/plugin/viewsets.py
+++ plugin/pulpcore/plugin/viewsets.py
@@ -1,3 +1,6 @@
+# ALlow plugin viewsets to return 202s
+from pulpcore.app.response import OperationPostponedResponse  # noqa
+
 # Import Viewsets in platform that are potentially useful to plugin writers
 from pulpcore.app.viewsets import (  # NOQA
     ContentViewSet,
